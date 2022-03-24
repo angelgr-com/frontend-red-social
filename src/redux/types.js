59b0d399a288111957df966351806205e14dfec2
RCD1985-GT <crazyspicefactory@gmail.com>
2022-03-24 19:41:03 +0100

    feat: redux/types.js

--- src/redux/types.js
+++ src/redux/types.js
@@ -2,5 +2,6 @@
 export const TEMA = 'tema';
 export const LOGIN = 'login';
 export const LOGOUT = 'logout';
-export const MODIFY_CREDENTIALS = 'modify_credentials';
+export const MODIFY_CREDENTIALS = 'modify_credentials'; 
+
 

3e71373e0503437eafbed3dc66f8b14ff3e8860d
Rodrigo <infohotsmoke@gmail.com>
2022-03-27 13:42:51 +0200

    feature: Profile.jsx updated

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -20,15 +20,15 @@ const Profile = (props) => {
     }, [])
 
     //Hooks
-    const [datosUsuario, setDatosUsuario] = useState({
-        nombre: props.credentials.usuario.nombre, apellido: props.credentials.usuario.apellido, edad: props.credentials.usuario.edad, email: props.credentials.usuario.email, 
-        nickname: props.credentials.usuario.nickname,  password: props.credentials.usuario.password
+    const [datosUser, setDatosUser] = useState({
+        name: props.credentials.user.name,  nickname: props.credentials.user.nickname, email: props.credentials.user.email, 
+        password: props.credentials.user.password,  avatar: props.credentials.user.avatar
     });
 
     //Handler (manejador)
     const rellenarDatos = (e) => {
         //para cambiar el hook
-        setDatosUsuario({...datosUsuario, 
+        setDatosUs({...datosUser, 
             [e.target.name]: e.target.value})
         //para cambiar el redux
         props.dispatch({ type: MODIFY_CREDENTIALS, payload: { field: e.target.name, field_value: e.target.value } })
@@ -43,12 +43,12 @@ const Profile = (props) => {
     const updateUser = async () => {
 
         let body = {
-            nombre: datosUsuario.nombre,
-            apellido: datosUsuario.apellido,
-            email: datosUsuario.email,
-            edad: parseInt(datosUsuario.edad),
-            nickname: datosUsuario.nickname,
-            password: datosUsuario.password
+            name: datosUser.name,
+            nickname: datosUser.nickname,
+            email: datosUser.email,
+            password: datosUser.password,
+            avatar: datosUser.avatar,
+            isAdmin: datosUser.isAdmin
             
         }
 
@@ -58,21 +58,14 @@ const Profile = (props) => {
 
         try {
             //Hacemos el update en la base de datos
-            let resultado = await axios.put(raiz +`usuarios/actualizar/perfilId/${props.credentials.usuario.id}`,body, config);
+            let resultado = await axios.put(raiz +`users/${props.credentials.user.id}`,body, config);
             setTimeout(() => {
                 // console.log("res2")
                 // console.log(res.data)
                 setRes(resultado.data); 
             }, 2);
 
-            // esto para ver la respeusta del put
-            // let res = await axios.put(`https://movie-db-geekshubs.herokuapp.com/usuarios/${props.credentials.usuario.id}`,body, config);
-            
-            
-            // if(res){
-            //     //Guardamos en redux
-            //     props.dispatch({type:MODIFY_CREDENTIALS, payload: datosUsuario});
-            // }
+          
         } catch (error) {
             console.log(error)
         }
@@ -82,18 +75,22 @@ const Profile = (props) => {
     return (
         <div className="designProfile">
             <div className="designProfileHalf profileLeft">
-                <div className="profileField"><b>Nombre:<input type="text" name="nombre" id="nombre" title="nombre" placeholder={props.credentials.usuario.nombre} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+
+                <div className="profileField"><b>Name:<input type="text" name="Name" id="Name" title="Name" placeholder={props.credentials.user.Name} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                 </b></div>
-                <div className="profileField"><b>Apellidos:</b><input type="text" name="apellido" id="apellido" title="apellido" placeholder={props.credentials.usuario.apellido} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                </div>
-                <div className="profileField"><b>Email:</b><input type="email" name="email" id="email" title="email" placeholder={props.credentials.usuario.email} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} /></div>
+                
                 <div className="profileField"><b>Nickname:</b><input type="text" name="nickname" id="nickname" title="nickname" placeholder={props.credentials.usuario.nickname} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                 </div>
-                <div className="profileField"><b>Edad:</b><input type="text" name="edad" id="edad" title="edad" placeholder={props.credentials.usuario.edad} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                </div>
-                
+
+                <div className="profileField"><b>Email:</b><input type="email" name="email" id="email" title="email" placeholder={props.credentials.usuario.email} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} /></div>
+
+                                
                 <div className="profileField"><b>Password:</b><input type="text" name="password" id="password" title="password" placeholder="*****"autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                 </div>
+
+                <div className="profileField"><b>Avatar:</b><input type="text" name="Avatar" id="Avatar" title="Avatar"  onChange={(e) => { rellenarDatos(e) }} />
+                </div>
+
                 <div className="profileFieldButton">
                     <div className="button type3 espacio" onClick={()=>updateUser()}>Actualiza</div>
                 </div>

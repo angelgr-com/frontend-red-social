ee0059a6ae01b2f3e1296d4f61428296635b2ed4
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-04-02 16:02:56 +0200

    Change wrong assignment in textarea NewComment

--- src/Containers/NewComment/NewComment.jsx
+++ src/Containers/NewComment/NewComment.jsx
@@ -4,11 +4,12 @@ import React, { useState, useEffect } from 'react';
 import './NewComment.css';
 import axios from 'axios';
 import { raiz } from '../../utiles';
-
+import {checkError} from '../../utiles';
 
 const NewComment = (props) => {
 
     console.log("entro en NewComment");
+    const [msgError, setMsgError] = useState("");
 
 
     // Hook
@@ -19,16 +20,35 @@ const NewComment = (props) => {
 
 
     const rellenarDatos = (e) => {
+        console.log(e.target.value)
         setComment({...comment, 
             [e.target.name]: e.target.value})
     };
 
     // Funcion new Post
     let navigate = useNavigate();
-    const newComment = async () => {
+    const dataComment = async () => {
 
         console.log("entra en la funcion que crea un nuevo comentario")
+        setMsgError("");
+        let error = "";
 
+        let arrayCampos = Object.entries(comment);
+        
+        // //1 comprobación de errores antes de enviar al backend
+
+
+
+        for(let elemento of arrayCampos){
+            error = checkError(elemento[0],elemento[1]);
+
+            if(error !== "ok"){
+                setMsgError(error);
+                return;
+            };
+        };
+
+        console.log("todo ha ido bien",comment)
         let body = {
             posts: [{
                 author:props.credentials.name,
@@ -39,7 +59,9 @@ const NewComment = (props) => {
 
 
         try {
-
+            console.log("elementos")
+            console.log(props);
+            console.log(body);
             let resultado = await axios.put(raiz + `Comments/comments/new/${props.title_url}`, body); // VERIFICAR ESTA LINEA
             console.log(resultado);
             setComment(resultado.data); // SE GUARDA EL RESULTADO EN EL HOOK
@@ -63,9 +85,9 @@ const NewComment = (props) => {
             <br />
             <p>TRAER AUTHOR POR PROPS</p>
             <br />
-            <label htmlFor="title">Title</label>
-            <textarea name="title" id="title" placeholder="write your comments:" autoComplete="off" rows="10" cols="50" onChange={(e)=>{rellenarDatos(e)}}></textarea>
-            <div className="buttonnewComment" onClick={()=>newComment()}>
+            <label htmlFor="content">content</label>
+            <textarea name="content" id="content" placeholder="write your comments:" autoComplete="off" rows="10" cols="50" onChange={(e)=>{rellenarDatos(e)}}></textarea>
+            <div className="buttonnewComment" onClick={()=>dataComment()}>
                         Registrar
              </div>
 

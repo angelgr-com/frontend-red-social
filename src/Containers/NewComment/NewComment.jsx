d9de5bf0b5f1c85846051eff217cfacb7fa189db
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-04-02 14:53:12 +0200

    Redefine handler NewComment

--- src/Containers/NewComment/NewComment.jsx
+++ src/Containers/NewComment/NewComment.jsx
@@ -3,19 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
 import './NewComment.css';
 import axios from 'axios';
-import {raiz} from '../../utiles';
+import { raiz } from '../../utiles';
 
 
 const NewComment = (props) => {
 
     console.log("entro en NewComment");
 
-    // Navegar
-    let navigate = useNavigate();
-
-    const navegar = () => {
-        navigate("/"); 
-    }
 
     // Hook
     const [comment, setComment] = useState({
@@ -23,40 +17,37 @@ const NewComment = (props) => {
     });
 
 
-    // UseEffect de montaje
-    useEffect(() => {
-        newComment();
-    }, [])
 
-    //Handler 
-//     const rellenarDatos = (e) => { // CAMBIAR
-//         setComment({...comment
-//             [e.target.comment]: e.target.value})
-// };
-
-
+    const rellenarDatos = (e) => {
+        setComment({...comment, 
+            [e.target.name]: e.target.value})
+    };
 
     // Funcion new Post
-
+    let navigate = useNavigate();
     const newComment = async () => {
 
         console.log("entra en la funcion que crea un nuevo comentario")
 
         let body = {
-            "posts": [{
-            author : "rodrigo",
-            content : "Lorem ipsum"
-        }]
-                    }
-                
+            posts: [{
+                author:props.credentials.name,
+                content:comment.content
+            }]
+        }
+
 
 
         try {
 
-            let resultado = await axios.put(raiz + `threads/comments/new/${props.title_url}`,body); // VERIFICAR ESTA LINEA
+            let resultado = await axios.put(raiz + `Comments/comments/new/${props.title_url}`, body); // VERIFICAR ESTA LINEA
             console.log(resultado);
             setComment(resultado.data); // SE GUARDA EL RESULTADO EN EL HOOK
             console.log("commentario guardado en hook")
+
+            setTimeout(()=>{
+                navigate("/posts");
+            },1000);
 
 
         } catch (error) {
@@ -64,26 +55,28 @@ const NewComment = (props) => {
         }
     }
 
-   
-        return ( // AÑADIR  HANDLER
-            <div className="designNewComment">
-                <p>CREA AQUI TU COMENTARIO</p>
-                <br />
-                <br />
-                <p>TRAER AUTHOR POR PROPS</p>
-                <br />
-                <textarea name="textarea" rows="10" cols="50">Write something here</textarea>
-                {/* <input className='inputComment' type="textArea" name="Comment" id="Comment" title="Comment" placeholder="Escribe aqui tu comentario:" autoComplete="off" /> */}
-                <input type="submit" />
+
+    return ( // AÑADIR  HANDLER
+        <div className="designNewComment">
+            <p>CREA AQUI TU COMENTARIO</p>
+            <br />
+            <br />
+            <p>TRAER AUTHOR POR PROPS</p>
+            <br />
+            <label htmlFor="title">Title</label>
+            <textarea name="title" id="title" placeholder="write your comments:" autoComplete="off" rows="10" cols="50" onChange={(e)=>{rellenarDatos(e)}}></textarea>
+            <div className="buttonnewComment" onClick={()=>newComment()}>
+                        Registrar
+             </div>
 
 
 
 
 
 
-            </div>
-        )
- 
+        </div>
+    )
+
 }
 
 
@@ -91,7 +84,8 @@ const NewComment = (props) => {
 
 
 export default connect((state) => ({
-    post: state.post
+    post: state.post,
+    credentials: state.credentials
 }))(NewComment);
 
 

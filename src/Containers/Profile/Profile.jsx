662621d23f50da50c58ab4bdeeed281f9f32bb4b
RafaOrti <rafaeloriolorticarrio@gmail.com>
2022-04-02 16:44:06 +0200

    fix register endpoint

--- src/Containers/Profile/Profile.jsx
+++ src/Containers/Profile/Profile.jsx
@@ -19,8 +19,11 @@ const Profile = (props) => {
    console.log(props.credentials.token)
     //Hooks
     const [datosUser, setDatosUser] = useState({
-        name: props.credentials.user.name,  nickname: props.credentials.user.nickname, email: props.credentials.user.email, 
-        password: props.credentials.user.password,  avatar: props.credentials.user.avatar
+        name: props.credentials.name,  
+        nickname: props.credentials.nickname, 
+        email: props.credentials.email, 
+        password: props.credentials.password,  
+        avatar: props.credentials.avatar
     });
 
     //Handler (manejador)
@@ -53,18 +56,17 @@ const Profile = (props) => {
             nickname: datosUser.nickname,
             email: datosUser.email,
             password: datosUser.password,
-            avatar: datosUser.avatar,
-            isAdmin: datosUser.isAdmin
+            avatar: datosUser.avatar
             
         }
 
-        let config = {
-            headers: { Authorization: `Bearer ${props.credentials.token}` }
-        };
+        // let config = {
+        //     headers: { Authorization: `Bearer ${props.credentials.token}` }
+        // };
 
         try {
             //Hacemos el update en la base de datos
-            let resultado = await axios.put(raiz +`users/${props.credentials.user.id}`,body, config);       
+            let resultado = await axios.put(raiz +`users/register`,body);       
             console.log("usuario actualizado enviado al backend")
             setDatosUser(resultado.data); 
             console.log("usuario actualizado guardado en Hook")
@@ -83,26 +85,22 @@ const Profile = (props) => {
         <div className="designProfile">
             <div className="designProfileInputs">
 
-                <div className="inputProfile"><b>Nombre:<input type="text" name="Name" id="Name" title="Name" placeholder={props.credentials.name} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                <div className="inputProfile"><b>Name:<input type="text" name="Name" id="Name" title="Name" placeholder={props.credentials.name} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                 </b></div>
                 
-                <div className="inputProfile"><b>Apellido:</b><input type="text" name="nickname" id="nickname" title="nickname" placeholder={props.credentials.nickname} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                </div>
+                <div className="inputProfile"><b>Nickname:</b><input type="text" name="email" id="email" title="email" placeholder={props.credentials.email} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} /></div> 
 
-                <div className="inputProfile"><b>Edad:</b><input type="text" name="email" id="email" title="email" placeholder={props.credentials.email} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} /></div>
 
                 <div className="inputProfile"><b>Email:</b><input type="email" name="email" id="email" title="email" placeholder={props.credentials.email} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} /></div>
 
-                <div className="inputProfile"><b>Nickname:</b><input type="text" name="email" id="email" title="email" placeholder={props.credentials.email} autoComplete="off" onChange={(e) => { rellenarDatos(e) }} /></div>               
+                              
 
-                <div className="inputProfile"><b>Contraseña:</b><input type="text" name="password" id="password" title="password" placeholder="*****"autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
-                </div>
-
-                <div className="inputProfile"><b>Repite Contraseña:</b><input type="text" name="password" id="password" title="password" placeholder="*****"autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
+                <div className="inputProfile"><b>Password:</b><input type="text" name="password" id="password" title="password" placeholder="*****"autoComplete="off" onChange={(e) => { rellenarDatos(e) }} />
                 </div>
 
                 <div className="inputProfile"><b>Avatar:</b><input type="text" name="Avatar" id="Avatar" title="Avatar"  onChange={(e) => { rellenarDatos(e) }} />
                 </div>
+
 
                 <div className="profileFieldButton">
                     <div className="buttonUpdate" onClick={()=>updateUser()}>Actualiza</div>
